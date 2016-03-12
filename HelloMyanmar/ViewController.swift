6c4efbb24b92e55672b530ae00ce68e5fe1fe01a
Nathan Sass <nsass711@gmail.com>
2016-03-12 07:44:31 +0630

    Added a tableview

--- HelloMyanmar/ViewController.swift
+++ HelloMyanmar/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +19,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "CafeLoisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh’s Chocolate", "Palomino Espresso",
+        "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle &Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina","Donostia", "Royal Oak", "CASK Pub and Kitchen"]
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cellIdentifier = "Cell"
+            let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+            // Configure the cell...
+            cell.textLabel!.text = restaurantNames[indexPath.row]
+            return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     @IBAction func showMessage(){
         let alertController = UIAlertController(title: "Mangala BAHH", message: "CHitteeh", preferredStyle: UIAlertControllerStyle.Alert)
